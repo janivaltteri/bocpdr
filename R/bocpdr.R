65a55ca4e0d92af9fad04a1f8faf5d15e742966a
Jani Valtteri Anttila <jva@localhost.localdomain>
2017-05-28 01:34:22 +0300

    output to matrix function added

--- R/bocpdr.R
+++ R/bocpdr.R
@@ -1,3 +1,4 @@
+## a function that calls the cpp routine for bocpd
 cpd <- function(steps,data,parsv){
     if(steps != length(data)) stop('steps has to equal length(data)')
     if(length(parsv) != 5) stop('parsv: c(gamma,kappa,alfa,beta,lambda)')
@@ -5,5 +6,20 @@ cpd <- function(steps,data,parsv){
     .Call('bocpd',c(steps),data,parsv,PACKAGE='bocpdr')
 }
 
-## add a wrapper for building the trellis object (triangular matrix)
+## a wrapper for building the trellis object (triangular matrix)
 ## from the output
+matrixform <- function(n,o){
+    mat <- matrix(0.0,nr=n,nc=n)
+    starts <- numeric(n); starts[1] <- 1
+    ends <- numeric(n)
+    for(i in 1:(n-1)){
+        ends[i] <- n - i + starts[i]
+        starts[i+1] <- ends[i] + 1
+    }
+    ends[n] <- (n+1)*n/2
+    for(i in 1:n){
+        mat[i,] <- c(numeric(i-1),o[starts[i]:ends[i]])
+    }
+    return(mat)
+}
+
